7fbec7672b6396d49c20138f4552fb0dec6da415
Ailsa Don <ailsazawadi415@gmail.com>
2025-12-16 19:06:01 +0300

    Added files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Index from './pages/Index';
 import About from './pages/About';
 import Signup from './pages/Signup';
+import ProtectedRoute from './components/ProtectedRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Index/>}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path='/about' element={<ProtectedRoute><About /></ProtectedRoute>}/>
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </Router>
